efc2335b8d0e3db61be714db5677d1a48767801e
NicolasGuruphat <nicolas.guruphat@gmail.com>
2022-01-05 15:38:24 +0100

    Création du package BaseDeDonnees et de la classe ConnectionFactory. Création des méthodes createConnection - qui permet de générer la connection (elle est ensuite stocké dans une variable static de la classe) - et getConnection

---
+++ GestionTournois/src/BaseDeDonnees/ConnectionFactory.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BaseDeDonnees;
+import java.sql.*;
+/**
+ *
+ * @author nicog
+ */
+public class ConnectionFactory {
+    
+        static Connection connection;
+        
+        public static Connection getConnetion(){
+            return connection;
+        }
+        
+        public static void createConnetion(){
+            try{
+                connection = DriverManager.getConnection("jdbc:mariadb://iutdoua-web.univ-lyon1.fr/p2003245?user=p2003245&password=564094");
+                connection.setAutoCommit(true);
+            }
+            catch(Exception e){
+                System.out.println(e); 
+            }
+        }
+}
