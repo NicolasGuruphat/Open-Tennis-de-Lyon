1594779dff1a1771cd18fbc6e98a92f9cff79f25
NicolasGuruphat <nicolas.guruphat@gmail.com>
2022-01-14 11:42:48 +0100

    Ajout du formulaire d'authentification

---
+++ CRM/index.php
@@ -0,0 +1,8 @@
+
+<form method='post' action="login.php">
+    <label for='futilisateur'>Nom d'utilisateur :</label><br>
+    <input type='text' id='futilisateur' name='utilisateur'><br><br>
+    <label for='fmotdepasse'>Mot de passe :</label><br>
+    <input type='password' id='fmotdepasse' name='motdepasse'><br><br>
+    <input type='submit' value='Se connecter'>
+</form>
