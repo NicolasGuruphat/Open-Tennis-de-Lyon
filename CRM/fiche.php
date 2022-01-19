db18647117cbf04bce6162ffa8e1b8b696f37078
NicolasGuruphat <nicolas.guruphat@gmail.com>
2022-01-19 15:09:48 +0100

    Adaptation du code à la nouvelle base de données

--- CRM/fiche.php
+++ CRM/fiche.php
@@ -13,10 +13,15 @@
 			<h2>Ajouter une interaction</h2>
 			
 			<form method='post' action="./fiche.php?id=<?=$_GET['id'];?>">
-				<label for='fobjet'>Objet :</label><br>
-				<input type='text' id='fobjet' name='objet' required><br><br>
-				<label for='fdescription'>Description :</label><br>
-				<input type='text' id='fdescription' name='description' required><br><br>
+				<label for='titre'>Titre :</label><br>
+				<input type='text' id='titre' name='titre' required><br><br>
+
+				<label for='status'>Status :</label><br>
+				<input type='text' id='status' name='status' required><br><br>
+
+				<label for='action'>Action :</label><br>
+				<input type='text' id='action' name='action' required><br><br>
+
 				<input type='submit' value='Submit'>
 			</form>
 	</div>
@@ -29,29 +34,53 @@
 		if($_COOKIE["token"]){
 			Connection::ConnectDb();
 			$bdd=Connection::getBDD();
-			$id=null;
+			
 			if(isset($_GET['id']))
 			{
 				$id=$_GET['id'];
-				$query = $bdd->prepare("SELECT * FROM FicheVIP where idFiche = $id");
-				$query->execute();
-				$data = $query->fetch();
-				echo "Identifiant : ".$data['0'];
+				
+				$query = $bdd->prepare("SELECT * from ListeVIP where idFiche=$id");
+        		$query->execute();
+				$data=$query->fetch();
+				if($data[1] != null){
+					$secondQuery = $bdd->prepare("SELECT nom, prenom, dateNaissance, nationalite from ListeVIP join Joueur where ListeVIP.idFiche=$id");
+				}
+				else if($data[2] != null){
+					$secondQuery = $bdd->prepare("SELECT nom, prenom, dateNaissance, nationalite from ListeVIP join Arbitre where ListeVIP.idFiche=$id");  
+				}
+				else if($data[4] != null){
+					$secondQuery = $bdd->prepare("SELECT nom, prenom, dateNaissance, nationalite from ListeVIP join RamasseurDeBalle where ListeVIP.idFiche=$id");           
+				}
+				else if ($data[5]!= null){
+					$secondQuery = $bdd->prepare("SELECT nom, prenom, dateNaissance, nationalite from ListeVIP join AutreVIP where ListeVIP.idFiche=$id");           
+				}
+				else{
+					$secondQuery = $bdd->prepare("SELECT * from Joueur");
+					echo "problème dans la table FicheVIP";
+				}
+				$secondQuery->execute();
+				$data2=$secondQuery->fetch();
+				echo "Identifiant : ".$id;
 				echo "</br>";
-				echo "Nom : ".$data['1'];
+				echo "Nom : ".$data2['0'];
 				echo "</br>";
-				echo "Prénom : ".$data['2'];
+				echo "Prénom : ".$data2['1'];
 				echo "</br>";
-				echo "Date de naissance : ".$data['3'];	
-
-				if(isset($_POST['objet']) and isset($_POST['description'])){
-					$titre=$_POST['objet'];
-					$description=$_POST['description'];
-					$query = $bdd->prepare("INSERT INTO Interaction (idFiche, idInteraction, titre, contenu, date) VALUES (?, ?, ?, ?, ?)");
-					$data=array($id,0,$titre,$description,date("Y-m-d"));	
+				echo "Date de naissance : ".$data2['2'];	
+				echo "</br>";
+				echo "Nationalité : ".$data2['3'];
+				if(isset($_POST['titre']) and isset($_POST['status']) and isset($_POST['action'])){
+					$titre=$_POST['titre'];
+					$status=$_POST['status'];
+					$action=$_POST['action'];
+					$query = $bdd->prepare("INSERT INTO Interaction (idInteraction, titre, date, status, action, idFiche) VALUES (?, ?, ?, ?, ?, ?)");
+					//$data=array($id,0,$titre,$description,date("Y-m-d"));	
+					$data=array(0,$titre, date("Y-m-d"),$status,$action,$id);
 					$query->execute($data);
+					$_POST = array();
 					echo "<p style='color:green'>votre interaction a été ajoutée avec succès</p>";
 				}
+
 
 				$query = $bdd->prepare("SELECT * FROM Interaction where idFiche = $id");
 				$query->execute();
@@ -61,12 +90,18 @@
 				{
 					echo "</br>";
 					echo "<div style=\"background-color: yellow\">";
-					$titre = $data['2'];
-					$contenu= $data['3'];
+					$titre = $data['1'];
+					$status = $data['3'];
+					$date= $data['2'];
+					$action = $data['4'];
 					
-					echo "objet : ".$titre;
+					echo "titre : ".$titre;
 					echo "</br>";
-					echo "contenu : ".$contenu;
+					echo "status : ".$status;
+					echo "</br>";
+					echo "date : ".$date;
+					echo "</br>";
+					echo "action : ".$action;
 					echo "</br>";
 					echo "</div>";
 				}
--- CRM/listeVIP.php
+++ CRM/listeVIP.php
@@ -5,19 +5,34 @@ if(isset($_COOKIE["token"])){
         echo "<a href=\"unlog.php\">Déconnexion</a>";
         Connection::ConnectDb();
         $bdd=Connection::getBDD();
-        $query = $bdd->prepare('SELECT nom,prenom,idFiche FROM FicheVIP');
+        $query = $bdd->prepare('SELECT * from ListeVIP');
         $query->execute();
-        while ($data = $query->fetch())  
-        {
-            $nom = $data['0'];
-            $prenom= $data['1'];
-            $id= $data['2'];
+        while($data=$query->fetch()){
+            if($data[1] != null){
+                $secondQuery = $bdd->prepare("SELECT nom,prenom,ListeVIP.idFiche from ListeVIP join Joueur where ListeVIP.idFiche=$data[0]");
+            }
+            else if($data[2] != null){
+                $secondQuery = $bdd->prepare("SELECT nom,prenom,ListeVIP.idFiche from ListeVIP join Arbitre where ListeVIP.idFiche=$data[0]");               
+            }
+            else if($data[3] != null){
+                $secondQuery = $bdd->prepare("SELECT nom,prenom,ListeVIP.idFiche from ListeVIP join RamasseurDeBalle where ListeVIP.idFiche=$data[0]");               
+            }
+            else if ($data[4]!= null){
+                $secondQuery = $bdd->prepare("SELECT nom,prenom,ListeVIP.idFiche from ListeVIP join AutreVIP where ListeVIP.idFiche=$data[0]");               
+            }
+            else{
+                echo "problème dans la table FicheVIP";
+            }
+            $secondQuery->execute();
+            $data2=$secondQuery->fetch();
+            $nom = $data2[0];
+            $prenom= $data2[1];
+            $id=$data[0];
             echo "<br><div style=\"background-color:cyan;border: 1px solid black;display:inline-block\">
             <a href=\"./fiche.php?id=$id\">
             $prenom $nom</a></div><br>";
             
         }
-        echo $data['0'];
     }
     else{
         header('Location:accessDenied.php');
