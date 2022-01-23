4a846b9685e4886b1b22487c6836f205c9c163fe
Hugo Torres <hugo.torres@etu.univ-lyon1.fr>
2022-01-23 20:14:53 +0100

    Starting implementing interface

--- GestionTournois/src/View/MatchsView.java
+++ GestionTournois/src/View/MatchsView.java
@@ -1,7 +1,11 @@
 package View;
 
+import Model.ObjectFactory;
+import Controller.Match;
+import Controller.Player;
 import javax.swing.*;
 import java.awt.*;
+import Model.MatchFactory;
 
 public class MatchsView {
     private JFrame tourSelect = new JFrame();
@@ -15,7 +19,6 @@ public class MatchsView {
     private void initComponents() {
         // Initialising both frames
         tourSelectInit();
-        matchesListInit();
         
         // We first use the tourSelect
         tourSelect.setVisible(true);
@@ -24,8 +27,10 @@ public class MatchsView {
         
     }
 
-    private void newWindow() {
+    private void newWindow(int round) {
         tourSelect.setVisible(false);
+        matchesList.getContentPane().removeAll();
+        matchesListInit(round);
         matchesList.setVisible(true);
         matchesList.repaint();
     }
@@ -43,28 +48,28 @@ public class MatchsView {
         
         JButton firstRound = new JButton("Premier Tour");
         firstRound.addActionListener(actionEvent -> {
+            newWindow(1);
             listTitle.setText("Matchs du premier tour");
-            newWindow();
         });
         JButton secondRound = new JButton("Second Tour");
         secondRound.addActionListener(actionEvent -> {
+            newWindow(2);
             listTitle.setText("Matchs du second tour");
-            newWindow();
         });
         JButton quarterFinal = new JButton("Quart de Finale");
         quarterFinal.addActionListener(actionEvent -> {
+            newWindow(3);
             listTitle.setText("Matchs de quart de finale");
-            newWindow();
         });
         JButton semiFinal = new JButton("Demi-finale");
         semiFinal.addActionListener(actionEvent -> {
+            newWindow(4);
             listTitle.setText("Matchs du demi-finale");
-            newWindow();
         });
         JButton finalRound = new JButton("Finale");
         finalRound.addActionListener(actionEvent -> {
+            newWindow(5);
             listTitle.setText("Match de finale");
-            newWindow();
         });
         
         tourSelect.add(firstRound);
@@ -75,7 +80,7 @@ public class MatchsView {
         tourSelect.setLayout(new FlowLayout());
     }
     
-    private void matchesListInit() {
+    private void matchesListInit(int round) {
         frameInit(matchesList, new Dimension(600, 800));
         listTitle = new JLabel();
         JButton backButton = new JButton("Retour");
@@ -85,8 +90,35 @@ public class MatchsView {
         });
         
         matchesList.add(listTitle);
+        createMatchList(round, matchesList);
         matchesList.add(backButton);
         matchesList.setLayout(new FlowLayout());
+    }
+    
+    private void createMatchList(int round, JFrame frame) {
+        ObjectFactory.createPlayers();
+        ObjectFactory.createReferee();
+        ObjectFactory.createBallPicker();
+        ObjectFactory.createSchedule();
+        ObjectFactory.createCourt();
+        MatchFactory.createMatch();
+        
+        for (Match m : Match.getListeMatch()) {
+            String player1 = "";
+            String player2 = "";
+            if (m.getTour() == round) {
+                for (Player p : m.getScore().keySet()) {
+                    if (player1.isEmpty()) {
+                        player1 = p.getLastName() + " " + p.getFirstName();
+                    } else {
+                        player2 = p.getLastName() + " " + p.getFirstName();
+                    }
+                }
+                JButton match = new JButton(player1 + " - " + player2);
+                frame.add(match); 
+            }
+        }
+        frame.repaint();
     }
 
     public static void main(String[] args) {
